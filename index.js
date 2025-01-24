c58e97ed0195d1620358e5e2751186a2cd83a9a4
PRIYANSHI-eng <priyanshi.chittora@kalvium.community>
2025-01-24 15:55:22 +0530

    add

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+// const port = 3010;
+
+
+// app.get('/', (req, res) => {
+//   res.sendFile(resolve(__dirname, 'pages/index.html'));
+// });
+
+app.get('/', (req, res) => {
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }
+});
+
+// app.listen(3000, () => console.log("Server is running on port 3000."));
+
+const isAdmin = process.env.IS_ADMIN === 'true';
+
+if (isAdmin) {
+  console.log("Admin privileges granted.");
+} else {
+  console.log("Access restricted. Admin only.");
+}
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`);
+});
